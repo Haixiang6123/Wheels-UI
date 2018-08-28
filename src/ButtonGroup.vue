4a904cd5294625db3c3a6bf2ac210f0a87ddc444
Haixiang <haixiang6123@gmail.com>
2018-08-28 23:07:04 +0800

    Fix margin-left bug

---
+++ src/ButtonGroup.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="ow-button-group">
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-group",
+        mounted() {
+            for (let node of this.$el.children) {
+                if (node.nodeName.toLowerCase() !== 'button') {
+                    console.warn(`Children of <ButtonGroup> should be <Button>, but <${node.nodeName.toLowerCase()}> detected!`);
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .ow-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        > .ow-button {
+            border-radius: 0;
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
---
+++ src/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg class="ow-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "icon",
+        props: ['name']
+    }
+</script>
+
+<style lang="scss">
+    .ow-icon {
+        height: 1em;
+        width: 1em;
+    }
+</style>
