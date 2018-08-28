bc7cf26e284e31f459f9e9d5d1d7bb7c2d747c5c
Haixiang <haixiang6123@gmail.com>
2018-08-28 20:41:30 +0800

    Remove .idea

---
+++ src/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="ow-button">Button</button>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style lang="scss">
+    .ow-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radis);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Button from './Button';
+
+Vue.component('ow-button', Button);
+
+new Vue({
+    el: '#app',
+    data: {
+    }
+})
