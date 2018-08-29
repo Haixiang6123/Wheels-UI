eb8681899e11f286070631f48e2c23e7f71f38f2
Haixiang <haixiang6123@gmail.com>
2018-08-29 21:40:37 +0800

    Add travis to project

---
+++ test/button.test.js
@@ -0,0 +1,78 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Button from '../src/Button'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Button', () => {
+    it('Button exist.', () => {
+        expect(Button).to.be.ok
+    });
+    it('可以设置icon.', () => {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings'
+            }
+        }).$mount()
+        const useElement = vm.$el.querySelector('use');
+        expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings');
+        vm.$destroy()
+    });
+    it('Enable loading button.', () => {
+        const Constructor = Vue.extend(Button);
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                loading: true
+            }
+        }).$mount();
+        const useElements = vm.$el.querySelectorAll('use');
+        expect(useElements.length).to.equal(1);
+        expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading');
+        vm.$destroy();
+    });
+    it('Default order of icon is 1', () => {
+        const div = document.createElement('div');
+        document.body.appendChild(div);
+        const Constructor = Vue.extend(Button);
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        }).$mount(div);
+        const icon = vm.$el.querySelector('svg');
+        expect(getComputedStyle(icon).order).to.eq('1');
+        vm.$el.remove();
+        vm.$destroy();
+    });
+    it('Set iconPosition to change order', () => {
+        const div = document.createElement('div');
+        document.body.appendChild(div);
+        const Constructor = Vue.extend(Button);
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right'
+            }
+        }).$mount(div);
+        const icon = vm.$el.querySelector('svg');
+        expect(getComputedStyle(icon).order).to.eq('2');
+        vm.$el.remove();
+        vm.$destroy();
+    });
+    it('Click button to trigger click event', () => {
+        const Constructor = Vue.extend(Button);
+        const vm = new Constructor({
+            propsData: {
+                icon: 'settings',
+            }
+        }).$mount();
+
+        const callback = sinon.fake();
+        vm.$on('click', callback);
+        vm.$el.click();
+        expect(callback).to.have.been.called;
+    });
+});
