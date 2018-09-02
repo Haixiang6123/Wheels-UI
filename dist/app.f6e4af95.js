d7a42c6549292fd32fa74c67b31985c772b06396
Haixiang <haixiang6123@gmail.com>
2018-09-02 21:32:15 +0800

    Add v-model doubly bind feature to Component Input

--- dist/app.f6e4af95.js
+++ dist/app.f6e4af95.js
@@ -10701,16 +10701,16 @@ exports.default = {
         domProps: { value: _vm.value },
         on: {
           change: function($event) {
-            _vm.$emit("change", $event)
+            _vm.$emit("change", $event.target.value)
           },
           input: function($event) {
-            _vm.$emit("input", $event)
+            _vm.$emit("input", $event.target.value)
           },
           focus: function($event) {
-            _vm.$emit("focus", $event)
+            _vm.$emit("focus", $event.target.value)
           },
           blur: function($event) {
-            _vm.$emit("blur", $event)
+            _vm.$emit("blur", $event.target.value)
           }
         }
       }),
@@ -10803,7 +10803,8 @@ new _vue2.default({
     data: {
         loading1: false,
         loading2: true,
-        loading3: false
+        loading3: false,
+        message: 'hi'
     },
     methods: {
         inputChange: function inputChange(e) {
