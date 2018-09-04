d168a6556bb2699f1e57eccc1086342786536dad
Haixiang <haixiang6123@gmail.com>
2018-09-04 15:52:48 +0800

    Add Layout, Footer, Content, Header

---
+++ src/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="heaer">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="layout">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "layout"
+    }
+</script>
+
+<style scoped>
+    .layout {
+
+    }
+</style>
