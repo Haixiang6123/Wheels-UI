6e9d4c8ba7f90c50eaf2d6ee1c38227c48e38748
Haixiang <haixiang6123@gmail.com>
2018-09-15 03:07:29 +0800

    Use vue-cli to construct project.

--- index.js
+++
@@ -1,9 +0,0 @@
-import Button from './src/Button';
-import ButtonGroup from './src/ButtonGroup';
-import Icon from './src/Icon';
-
-export {
-    ButtonGroup,
-    Button,
-    Icon
-}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Button from './Button';
+import ButtonGroup from './ButtonGroup';
+import Icon from './Icon';
+
+export {
+    ButtonGroup,
+    Button,
+    Icon
+}
